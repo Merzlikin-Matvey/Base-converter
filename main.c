6bf043dd9ed9ad6168567153e2c992bc518c5fae
Merzlikin-Matvey <merzlikinmatvey01@gmail.com>
2024-02-22 23:35:08 +0300

    Перевод из системы base1 в base2

--- main.c
+++ main.c
@@ -14,26 +14,55 @@ char* get_alphabet(int n) {
     return result;
 }
 
-// Перевод из десятичной системы счисления в систему счисления с основанием base
-char* base_convert(int n, int base){
+// Перевод из системы счисления с основанием base в десятичную систему счисления
+int convert_to_dec(char* number, int base){
+    if (base > 62){
+        return -1;
+    }
+
+    int len = strlen(number);
     char* alphabet = get_alphabet(base);
-    int len = 1;
-    int tmp = n;
-    while (tmp >= base) {
-        tmp /= base;
-        len++;
-    }
-
-    char* result = malloc((len + 1) * sizeof(char));
-    result[len] = '\0';
-
+    int result = 0;
+    int pow = 1;
     for (int i = len - 1; i >= 0; i--) {
-        result[i] = alphabet[n % base];
-        n /= base;
+        int index = 0;
+        for (int j = 0; j < base; j++) {
+            if (number[i] == alphabet[j]) {
+                index = j;
+                break;
+            }
+        }
+        result += index * pow;
+        pow *= base;
     }
-
     return result;
 }
+
+// Перевод из системы счисления с основанием base1 в систему с основанием base2
+char* base_converter(char* number, int base1, int base2){
+    if (base1 > 62 || base2 > 62){
+        return NULL;
+    }
+
+    int dec = convert_to_dec(number, base1);
+    char* alphabet = get_alphabet(base2);
+    int len = 0;
+    int tmp = dec;
+    while (tmp > 0) {
+        tmp /= base2;
+        len++;
+    }
+    if (len == 0) len = 1;
+    char* result = malloc((len + 1) * sizeof(char));
+    result[len] = '\0';
+    for (int i = len - 1; i >= 0; i--) {
+        result[i] = alphabet[dec % base2];
+        dec /= base2;
+    }
+    return result;
+}
+
+
 
 
 
